80ad36a16d536d3791ea404446fe6cf4020a2a4b
Дмитрий Гулевич <kaskodim@gmail.com>
2024-04-14 10:41:47 +0300

    правки

--- function/onPressOrOnClickNumber.js
+++ function/onPressOrOnClickNumber.js
@@ -8,23 +8,20 @@ function onPressOrOnClickNumber(num) {
         input.textContent = '';
     }
     if (mathematicSign === '' || oneNumber === '') {
-        if (oneNumber.includes('.')) {
-            if (oneNumber.split('.')[1].length === 3) {
-                changeScreenText(oneNumber);
-                return
-            }
+
+        if (oneNumber.includes('.') && oneNumber.split('.')[1].length === 3) {
+            return
         }
+
         oneNumber = oneNumber + num;
         oneNumber = (Math.round(+oneNumber * 1000) / 1000).toString();
         changeScreenText(oneNumber);
     }
     else {
-        if (twoNumber.includes('.')) {
-            if (twoNumber.split('.')[1].length === 3) {
-                changeScreenText(twoNumber);
-                return
-            }
+        if (twoNumber.includes('.') && twoNumber.split('.')[1].length === 3) {
+            return
         }
+
         twoNumber = twoNumber + num;
         twoNumber = (Math.round(+twoNumber * 1000) / 1000).toString();
         changeScreenText(twoNumber);
